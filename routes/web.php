4d5249943096bdf8e784660665c297508418aabb
shalinit3 <shainityagi64@gmail.com>
2025-07-15 04:03:04 -0400

    feat: cleanup menu and setup initial routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::get('/companies', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
