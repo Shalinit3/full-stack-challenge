8acc4fbc643780ac71dac9ae6b9e43e493ece3a5
shalinit3 <shainityagi64@gmail.com>
2025-07-15 04:11:59 -0400

    chore: setup jest

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/resources/js/$1',
+  },
+  testMatch: ['**/**/*.test.(ts|tsx)'],
+};
+
+export default config;
